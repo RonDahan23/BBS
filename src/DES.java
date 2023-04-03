077765f5163e0a1e0c733ba1314c9b29d98dfe59
Ron <rondahan2016@gmail.com>
2023-04-03 22:55:39 +0300

    second version

--- src/DES.java
+++ src/DES.java
@@ -21,9 +21,6 @@ public class DES {
         return paddedBinaryString;
     }
 
-
-
-
     public static Hashtable<Integer, Character> InsertHashTable(String str, Hashtable<Integer, Character> hashTable) {
         for (int i = 0; i < str.length(); i++) {
             hashTable.put(i, str.charAt(i));
@@ -31,10 +28,10 @@ public class DES {
         return hashTable;
     }
 
-    public static String Generating56Bits(Hashtable<Integer, Character> hashtable, Integer[] array) {
+    public static String Generating56Bits(Hashtable<Integer, Character> hashtable, Integer[] PC1) {
         StringBuilder strGenerating56Bits = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            char value = hashtable.get(array[i] - 1);
+        for (int i = 0; i < PC1.length; i++) {
+            char value = hashtable.get(PC1[i] - 1);
             strGenerating56Bits.append(value);
         }
         return strGenerating56Bits.toString();
@@ -45,5 +42,39 @@ public class DES {
         String[] parts = {str.substring(0, mid), str.substring(mid)};
         return parts;
     }
+    public static String Do16LeftShift(String right, String left, Integer[] NumberOfLeftShifts, Integer[] PC2)
+    {
+        String Text ="";
+        for(int i = 0; i < NumberOfLeftShifts.length; i++)
+        {
+
+            left = left.substring(NumberOfLeftShifts[i]) + left.substring(0, NumberOfLeftShifts[i]);
+            right = right.substring(NumberOfLeftShifts[i]) + right.substring(0, NumberOfLeftShifts[i]);
+            Text = left + right;
+            Text = Generating48Bits(Text, PC2);
+            System.out.println(Text);
+
+        }
+
+
+        return Text;
+    }
+    public static String Generating48Bits(String str, Integer[] PC2)
+    {
+        Hashtable<Integer, Character> hashTable48 = new Hashtable<>();
+        hashTable48 = InsertHashTable(str, hashTable48);
+
+        StringBuilder strGenerating56Bits = new StringBuilder();
+        for (int i = 0; i < PC2.length; i++) {
+            Character value = hashTable48.get(PC2[i] - 1);
+            if (value != null) {
+                strGenerating56Bits.append(value.charValue());
+            }
+        }
+
+        return strGenerating56Bits.toString();
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
-    static Hashtable<Integer, Character> hashTable = new Hashtable<>();
+    static Hashtable<Integer, Character> hashTable56 = new Hashtable<>();
+
     static Integer[] PC1 = {57,49,41,33,25,17,9,1,
             58,50,42,34,26,18,10,2,
             59,51,43,35,27,19,11,3,
@@ -11,17 +14,28 @@ public class Main {
             30,22,14,6,61,53,45,37,
             29,21,13,5,28,20,12,4};
 
+    static Integer[] PC2 = {
+            14, 17, 11, 24, 1, 5, 3, 28,
+            15, 6, 21, 10, 23, 19, 12, 4,
+            26, 8, 16, 7, 27, 20, 13, 2,
+            41, 52, 31, 37, 47, 55, 30, 40,
+            51, 45, 33, 48, 44, 49, 39, 56,
+            34, 53, 46, 42, 50, 36, 29, 32
+    };
+
+    static Integer[] NumberOfLeftShifts = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+
     public static void main(String[] args) {
 
         DES d = new DES();
         String a = "IEOFIT#1";
-        hashTable = d.InsertHashTable(d.stringToBinary(a), hashTable);
+        hashTable56 = d.InsertHashTable(d.stringToBinary(a), hashTable56);
 
-        String strGenerating56Bits = d.Generating56Bits(hashTable, PC1);
+        String strGenerating56Bits = d.Generating56Bits(hashTable56, PC1);
         String[] parts = d.CutInTheMiddle(strGenerating56Bits);
         String left = parts[0];
         String right = parts[1];
-        System.out.println(left + right);
+        d.Do16LeftShift(right, left, NumberOfLeftShifts, PC2);
 
     }
 
